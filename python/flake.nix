b88efcde56caccdb9b4e0dc8cd5fce4b575d3c4a
Tamipes <tamipesagniyah@gmail.com>
2024-10-17 19:07:15 +0200

    apply the fix to python shell flake.nix as well.

--- python/flake.nix
+++ python/flake.nix
@@ -6,8 +6,8 @@
 
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = nixpkgs { inherit system; }; in {
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = import nixpkgs { inherit system; }; in {
 
       devShells.default = pkgs.mkShell {
         packages = [
